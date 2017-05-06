b47fbb011ff7c7804a2483f28550a2687de5fa06
Nic Roland <nicroland9@gmail.com>
2017-05-06 13:21:24 +0100

    Switching from POST form to GET w/ qstring

--- groupie.py
+++ groupie.py
@@ -3,13 +3,9 @@ import os
 import json
 
 from urllib.parse import urljoin
-
-from flask import Flask, render_template, redirect, flash, request, abort
-from wtforms import TextField, PasswordField, SelectField
-from flask_wtf import Form
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.secret_key = 'myverylongsecretkey'
 
 BASE_URL = "https://app.ticketmaster.com/"
 
@@ -18,10 +14,8 @@ if not TICKETMASTER_API_KEY:
     print(USAGE)
     exit(1)
 
-## Forms
 
-class AttractionSearch(Form):
-    keyword = TextField('Search')
+# Helper methods
 
 def search_attractions(keyword, locale='en-ie'):
     url = urljoin(BASE_URL, '/discovery/v2/attractions.json')
@@ -34,20 +28,14 @@ def search_attractions(keyword, locale='en-ie'):
 
 ## Routes
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def homepage():
-    form = AttractionSearch()
-    if form.validate_on_submit():
-        attractions = search_attractions(form.keyword.data)
-        return attractions
-    return render_template('homepage.html', form=form)
+    return render_template('homepage.html')
 
-@app.route('/attractions', methods=['POST'])
-def attraction_search():
-    form = AttractionSearch()
-    if form.validate_on_submit():
-        results = search_attractions(form.keyword.data)
-        return render_template('attractions.html', attractions=results['_embedded']['attractions'])
+@app.route('/attractions', methods=['GET'])
+def attractions():
+    results = search_attractions(request.args.get('q'))
+    return render_template('attractions.html', attractions=results['_embedded']['attractions'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
